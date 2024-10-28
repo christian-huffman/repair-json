da606629c94ff21509608cd610dcba9128ef2b1a
christian-huffman <christian.huffman.88@gmail.com>
2024-10-28 17:57:51 +0900

    feat/added index.js

---
+++ index.js
@@ -0,0 +1,66 @@
+const jsonData = {
+    key1: "val1",
+    key2: { key3: "val2" },
+    key4: "val4"
+}
+
+function repairJson(input) {
+    if (input[input.length - 1] === "}") {
+        let openBraceCnt = (input.match(/{/g) || []).length
+        let closeBraceCnt = (input.match(/}/g) || []).length
+        while (openBraceCnt > closeBraceCnt) {
+            input += "}"
+            closeBraceCnt++
+        }
+        return input
+    } else if (input[input.length - 1] === '"') {
+        if (input.slice(-2) === ':"') {
+            return repairJson(input + 'unknown"')
+        } else if (input.slice(-2) === '{"') {
+            return repairJson(input + 'default":"unknown"}')
+        } else if (input.slice(-2) === ',"') {
+            return repairJson(input.slice(0, -2))
+        } else {
+            for (let i = input.length - 2; i >= 0; i--) {
+                if (input[i] === '"' && input[i - 1] === ":") {
+                    return repairJson(input + "}")
+                } else if (input[i] === '"' && input[i - 1] === ",") {
+                    return repairJson(input + ':"unknown"}')
+                } else if (input[i] === '"' && input[i - 1] === "{") {
+                    return repairJson(input + ':"unknown"}')
+                }
+            }
+        }
+    } else if (input.slice(-1) === "{") {
+        return repairJson(input + '"default":"unknown"}')
+        // if (input.slice(-2) === ":{") {
+        //     return repairJson(input.slice + '"default":"unknown"}')
+        // } else {
+        //     return repairJson(input.slice(0, -1) + '"unknown"')
+        // }
+    } else if (input.slice(-2) === '":') {
+        return repairJson(input + '"unknown"}')
+    } else if (input.slice(-2) === '",') {
+        return repairJson(input.slice(0, -1) + "}")
+    } else {
+        return repairJson(input + '"')
+    }
+    // console.log(input)
+    // console.log(JSON.parse(input))
+}
+
+// '{"key1":"val1","key2":{"key3":"val3"},"key4":"val4"
+// '{"key1":"val1","key2":{"key3":"val3"},"key4"
+// '{"key1":"val1","key2":{"key3":"val3"
+// '{"key1":"val1","key2":{"key3"
+// '{"key1":"val1","key2":{"key3":"val2"}'
+// '{"key1":"val1","key2":{"key3":"val2'
+// '{"key1":"val1","key2":{'
+// '{"key1":"val1","key2":{"key3":"'
+// '{"key1":"val1","key2"'
+// '{"key1":"val1"'
+// '{"key1":"val1","key2":{"key3"'
+// '{"key1":"val1","key2":{"'
+const sample = '{"key1":"val1","key2":{"key3":"'
+console.log(repairJson(sample))
+console.log(JSON.parse(repairJson(sample)))
